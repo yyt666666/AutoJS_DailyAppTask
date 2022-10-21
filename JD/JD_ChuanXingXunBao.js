e97d991bf0590fc80211bd4c781bc710e533e8eb
yyt666666 <47512829+yyt666666@users.noreply.github.com>
2022-10-21 09:21:04 +0800

    20221021 V1.2   修复打卡任务

--- JD/JD_ChuanXingXunBao.js
+++ JD/JD_ChuanXingXunBao.js
@@ -7,6 +7,8 @@
   20221021 V1.1
   修改去完成点击方式，如果手机不能完整显示任务列表则脚本运行可能报错
   增加新的弹窗处理
+  20221021 V1.2
+  修复打卡任务
 
 */
 var TaskName = "穿行寻宝"
@@ -642,7 +644,7 @@ function Run(LauchAPPName, IsSeparation, IsInvite, IsJoinMember) {
       IsJoinMember = 0
       console.log("已连续" + IsNotJoinMemberTimes + "次为新店铺，跳过入会任务");
     }
-    let taskButtons = textMatches(/.*浏览.*s.*|.*浏览.*秒.*|.*首页二屏.*|.*去手Q频道.*|.*去下游参加游戏.*|.*累计浏览.*|.*浏览加购.*|.*预约并浏览.*|.*浏览即可得.*|.*浏览并关注.*|.*逛会场可得.*|.*浏览可得.*|.*预约并了解.*|.*成功入会.*|.*小程序.*|.*去组队可得.*|.*每日6-9点打卡可得.*|.*去APP.*|.*参与城城点击.*|.*品牌墙店铺.*|.*玩AR游戏可得.*金币.*/).find()
+    let taskButtons = textMatches(/.*浏览.*s.*|.*浏览.*秒.*|.*首页二屏.*|.*去手Q频道.*|.*去下游参加游戏.*|.*累计浏览.*|.*浏览加购.*|.*预约并浏览.*|.*浏览即可得.*|.*浏览并关注.*|.*逛会场可得.*|.*浏览可得.*|.*预约并了解.*|.*成功入会.*|.*小程序.*|.*去组队可得.*|.*打卡可得.*|.*去APP.*|.*参与城城点击.*|.*品牌墙店铺.*|.*玩AR游戏可得.*金币.*/).find()
     if (taskButtons.empty()) {
       console.log("未找到合适的任务，退出");
       sleep(3000);
@@ -1598,7 +1600,7 @@ function Run(LauchAPPName, IsSeparation, IsInvite, IsJoinMember) {
         console.log("任务完成");
         IsNotJoinMemberTimes = 0;
       }
-    } else if (taskText.match(/预约并了解|玩AR游戏可得|每日6-9点打卡/)) {
+    } else if (taskText.match(/预约并了解|玩AR游戏可得|打卡/)) {
       console.log(taskText);
       //taskButton.click();
       sleep(2000);
